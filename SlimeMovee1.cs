cc8cf677778c850184f824e0a1ba45942da8c476
JeongSeonmi <pm3818@naver.com>
2023-05-08 01:13:53 +0900

    첫번째 commit

---
+++ SlimeMovee1.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeMovee1 : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    public int nextMove;
+    void Awake()
+    {
+        
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Invoke("Think", 3); //주어진 시간이 지난 뒤, 지정된 함수를 실행하는 함수
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove*2, rigid.velocity.y);  //velocity는 rigid꺼인데 웬만한 물리적인건 Rigidbody에 있음
+
+        //Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.8f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform")); //GetMask(): 레이어 이름에 해당하는 정수값을 리턴하는 함수
+        if(rayHit.collider == null){
+            Turn(); //방향이 반대
+        }
+    }
+
+    void Think()  //재귀함수
+    {
+        //Set Next Active
+        nextMove = Random.Range(-1, 2); //최대는 포함X)
+
+        //Sprite Animation
+        animator.SetInteger("WalkSpeed", nextMove);
+
+        //Flip Sprite
+        if(nextMove !=0)
+            spriteRenderer.flipX = nextMove == 1; 
+            
+        //Recursive    
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn(){
+        nextMove *= (-1);
+        spriteRenderer.flipX = nextMove == 1; 
+        CancelInvoke();
+        Invoke("Think", 3);
+    }
+}
---
+++ swordman.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swordman : MonoBehaviour
+{
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        /*Jump인데요*/
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("jumping")){
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("jumping", true);
+        }
+        
+
+        /*Stop Speed*/
+        if (Input.GetButtonUp("Horizontal")){
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x*0.5f, rigid.velocity.y);
+        }
+
+        /*Direction Sprite(방향 전환)*/
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        /*Animation(달릴 때, 애니메이션)*/
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)   //Mathf : 수학 관련 함수를 제공하는 클래스 
+            animator.SetBool("moving", false);
+        else
+            animator.SetBool("moving", true);
+        
+    }
+    void FixedUpdate()
+    {
+        /*Move Speed*/
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        /*Max Speed*/
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y); // Right Max Speed
+        else if (rigid.velocity.x < maxSpeed * (-1))
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y); // Left Max Speed
+
+        /*Landing Platform*/
+        if(rigid.velocity.y < 0){
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0)); //DrawRay() : 에디터 상에서만 Ray를 그려주는 함수, 색깔은 그냥 그린
+            //빔을 쏴서 맞는걸 확인, 정확한 판단을 위해서 거리(길이?) 설정
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position*0.5f, Vector3.down, 1, LayerMask.GetMask("Platform")); //GetMask(): 레이어 이름에 해당하는 정수값을 리턴하는 함수
+            if(rayHit.collider != null){
+                if(rayHit.distance < 0.6f) animator.SetBool("jumping", false);
+            }
+        }
+    }
+}
